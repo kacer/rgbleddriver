920114a6e31ee1f0d2e0b940d75deb059847182e
kacer <m.d.z.s@seznam.cz>
2019-12-14 00:01:59 +0100

    Přidána možnost vytvoření barvy na základě hex color stringu
    Overridnuta metoda equals

--- src/rgbleddriver/model/Color.java
+++ src/rgbleddriver/model/Color.java
@@ -1,5 +1,8 @@
 package rgbleddriver.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Color {
 	
 	public static final Color BLACK = new Color(0, 0, 0);
@@ -72,5 +75,33 @@ public class Color {
 	public String getRGBString() {
 		return "rgb(" + r +", " + g + ", "+ b + ")";
 	}
+	
+	public static Color fromString(String hexColor) {
+		Pattern p = Pattern.compile("^#(?:[0-9a-fA-F]{3}){1,2}$");
+		Matcher m = p.matcher(hexColor);
+		if(!m.find())
+			throw new IllegalArgumentException("Invalid hex color string: " + hexColor);
+		
+		String safeStr = m.group();
+		int r = Integer.valueOf(safeStr.substring(1, 3), 16);
+		int g = Integer.valueOf(safeStr.substring(3, 5), 16);
+		int b = Integer.valueOf(safeStr.substring(5, 7), 16);
+		
+		return new Color(r, g, b);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(!(obj instanceof Color))
+			return false;
+		
+		Color c = (Color) obj;
+		return (r == c.r) && (g == c.g) && (b == c.b);
+	}
+	
+	
 
 }
